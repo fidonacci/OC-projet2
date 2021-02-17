955dcdb3152caf3a2f9678193785e95714c7a302
fidonacci <fahd.jamai@outlook.com>
2021-02-17 15:02:50 +0100

    Docstring added to all functions + Timer to monitor parsing performance of each request

--- main.py
+++ main.py
@@ -1,45 +1,61 @@
+""" This module is the main module from which the program is lanched """
 
 import csv
 import os
 import requests
 from scrap_functions import scrap_book, scrap_categories, scrap_category_books
 from constants import url_home, csv_columns
+import time
 
 
 csv_files_directory = os.getcwd() + '/scraping_files_exercice/'
 print(csv_files_directory)
-csv_images_directory = os.path.join(csv_files_directory,'images/') 
+csv_images_directory = os.path.join(csv_files_directory, 'images/')
 print(csv_images_directory)
 
 try:
-    os.makedirs(csv_images_directory,exist_ok=True)
-    
+    os.makedirs(csv_images_directory, exist_ok=True)
+
 except OSError:
     print('Le dossier csv existe')
 
 if __name__ == '__main__':
 
-    for categorie in scrap_categories(url_home):
+    tic = time.perf_counter()
+    scraped_categories = scrap_categories(url_home)
+    toc = time.perf_counter()
+    print(f"scraped_categories in {toc - tic:0.4f} seconds")
+
+    for categorie in scraped_categories:
 
         csv_file = os.getcwd() + '/scraping_files_exercice/' + \
             categorie[0] + ".csv"
 
         with open(csv_file, 'w+') as csvfile:
 
-            writer = csv.DictWriter(csvfile,delimiter=';', fieldnames=csv_columns)
+            writer = csv.DictWriter(
+                csvfile, delimiter=';', fieldnames=csv_columns)
             writer.writeheader()
 
+            tic = time.perf_counter()
+            scraped_category_books = scrap_category_books(categorie[1])
+            toc = time.perf_counter()
+            print(f"scraped_category_books of {categorie[1]} in {toc - tic:0.4f} seconds")
 
-            for book in scrap_category_books(categorie[1]):
-
-                scrap = scrap_book(book)
-
-                writer.writerow(scrap)
-
-                myfile = requests.get(scrap['image_url'])
-                myfile_name = scrap['title'].replace('#','').replace(',',' ').replace(':','-').replace('.',' ').replace('/','-')
-
-                open(csv_images_directory+ myfile_name   +
-                     '.png', 'wb').write(myfile.content)
+            for book in scraped_category_books:
                 
-                print(scrap['category'], scrap['title'])
+                tic = time.perf_counter()
+                scraped_book = scrap_book(book)
+                toc = time.perf_counter()
+                print(f"scraped_book of {scraped_book['title']} in {toc - tic:0.4f} seconds")
+
+                writer.writerow(scraped_book)
+
+                myfile = requests.get(scraped_book['image_url'])
+                myfile_name = scraped_book['title'].replace('#', '').replace(
+                    ',', ' ').replace(':', '-').replace('.', ' ').replace('/', '-')
+
+                open(csv_images_directory + myfile_name +
+                     '.png', 'wb').write(myfile.content)
+
+                print(scraped_book['category'], scraped_book['title'])
--- scrap_functions.py
+++ scrap_functions.py
@@ -1,37 +1,61 @@
+""" This module encompasses all the functions needed to scrap the books website """
+
 import requests
 from bs4 import BeautifulSoup
 import re
+from constants import url_home
 
 # définitions
-categories_list=[]
-book={}
-
-# url de la page home du site à scrapper
-url_home = 'http://books.toscrape.com/'
+categories_list = []
+book = {}
 
 # détermination des informations d'une page d'un livre
-#---------------------------------------------------------
+# ---------------------------------------------------------
+
+
 def scrap_book(url):
-    
+    """Return a dict with book informations.
+
+    Keyword arguments:
+    url -- the book page url (no default)
+
+    """
+
     if requests.get(url).ok:
         response = requests.get(url)
         page_book = BeautifulSoup(response.text, 'lxml')
-        book["universal_product_code"] = page_book.table.find_all('tr')[0].td.text.encode('utf-8').decode('ascii', 'ignore')
-        book["title"] = page_book.h1.text.encode('utf-8').decode('ascii', 'ignore')
-        book["price_including_tax"] = page_book.table.find_all('tr')[3].td.text.encode('utf-8').decode('ascii', 'ignore')
-        book["price_excluding_tax"] = page_book.table.find_all('tr')[2].td.text.encode('utf-8').decode('ascii', 'ignore')
-        book["number_available"] = int(re.findall('[0-9]+', str(page_book.table.find_all('tr')[5]))[0])
-        book["product_description"] = page_book.find_all('p')[3].text.encode('utf-8').decode('ascii', 'ignore')
-        book["category"] = page_book.find(class_='breadcrumb').find_all('a')[2].text.encode('utf-8').decode('ascii', 'ignore')
+        book["universal_product_code"] = page_book.table.find_all(
+            'tr')[0].td.text.encode('utf-8').decode('ascii', 'ignore')
+        book["title"] = page_book.h1.text.encode(
+            'utf-8').decode('ascii', 'ignore')
+        book["price_including_tax"] = page_book.table.find_all(
+            'tr')[3].td.text.encode('utf-8').decode('ascii', 'ignore')
+        book["price_excluding_tax"] = page_book.table.find_all(
+            'tr')[2].td.text.encode('utf-8').decode('ascii', 'ignore')
+        book["number_available"] = int(re.findall(
+            '[0-9]+', str(page_book.table.find_all('tr')[5]))[0])
+        book["product_description"] = page_book.find_all(
+            'p')[3].text.encode('utf-8').decode('ascii', 'ignore')
+        book["category"] = page_book.find(class_='breadcrumb').find_all(
+            'a')[2].text.encode('utf-8').decode('ascii', 'ignore')
         book["review_rating"] = " ".join(page_book.find_all('p')[2]['class'])
-        book["image_url"] = "http://books.toscrape.com/"+page_book.find_all('img')[0]['src'].strip('../../').encode('utf-8').decode('ascii', 'ignore')
-    
+        book["image_url"] = "http://books.toscrape.com/"+page_book.find_all(
+            'img')[0]['src'].strip('../../').encode('utf-8').decode('ascii', 'ignore')
+
     return book
 
-# détermination des catégories existantes / renvoie une liste 
-#---------------------------------------------------------
+# détermination des catégories existantes / renvoie une liste
+# ---------------------------------------------------------
+
+
 def scrap_categories(url):
-    
+    """Return a list of lists formed as [category name, category url].
+
+    Keyword arguments:
+    url -- the site home page url (no default)
+
+    """
+
     if requests.get(url).ok:
         response = requests.get(url)
         page_accueil = BeautifulSoup(response.text, 'lxml')
@@ -40,58 +64,67 @@ def scrap_categories(url):
         categories = page_accueil.find(class_='nav nav-list').find_all('a')
         categories.pop(0)
         for a in categories:
-            categories_list.append([a.text.strip(),url+a['href']])
-    
+            categories_list.append([a.text.strip(), url+a['href']])
+
     return categories_list
 
 # renvoie un tableau de l'ensemble des urls des livres de toutes les pages de la catégorie
 # url pris en entrée est l'url "home" de la catégorie
-#---------------------------------------------------------
+# ---------------------------------------------------------
+
+
 def scrap_category_books(url):
-    
-    category_books=[]
+    """Return a list of books urls of the whole category.
+
+    Keyword arguments:
+    url -- the category home page url (no default)
+
+    """
+
+    category_books = []
 
     if requests.get(url).ok:
         response = requests.get(url)
         category_home = BeautifulSoup(response.text, 'lxml')
 
-    #s'il n'y pas de bouton next on s'arrête à la page home, sinon on parcourt les url de toutes les catégories
-    if category_home.find(class_='next')==None:
-        
-        category_books= scrap_page_books(url)
+    # s'il n'y pas de bouton next on s'arrête à la page home, sinon on parcourt les url de toutes les catégories
+    if category_home.find(class_='next') == None:
+
+        category_books = scrap_page_books(url)
         return category_books
 
     else:
-        j=1
+        j = 1
         category_page_url = url.rstrip('/index.html')+f'/page-{j}.html'
-        
+
         while requests.get(category_page_url).status_code == 200:
-            
+
             category_books.extend(scrap_page_books(category_page_url))
-            j+=1
+            j += 1
             category_page_url = url.rstrip('/index.html')+f'/page-{j}.html'
-        
+
         return category_books
 
 # fonction qui detrmine les blocs de page / renvoie un tableau de url des livres affichés / page en argument est une url
-#---------------------------------------------------------
+# ---------------------------------------------------------
+
+
 def scrap_page_books(url):
-       
+    """Return a list of books urls of the specific category page.
+
+    Keyword arguments:
+    url -- the category page url (no default)
+
+    """
     if requests.get(url).ok:
         response = requests.get(url)
         page = BeautifulSoup(response.text, 'lxml')
-        
+
     page_books = page.find_all(class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
     url_table = []
 
     for p in page_books:
-        url_table.append(url_home + 'catalogue/'+p.h3.a['href'].strip('../../../'))
-    
+        url_table.append(url_home + 'catalogue/' +
+                         p.h3.a['href'].strip('../../../'))
+
     return url_table
-
-
-
-
-scrap_book('http://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html')   
-
-
